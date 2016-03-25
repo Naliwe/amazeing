f87ab95b26d95c2aab41d160c7c4978890c96e27
naliwe <r.fourdrilis@gmail.com>
2016-03-25 18:45:34 +0100

    Switch

--- main.ml
+++
@@ -1,7 +0,0 @@
-module PerfectLabyrinth = Labyrinth.MakeLabyrinth (Cell.SquareCell) (Wall.SimpleWall)
-
-let main () =
-  let laby = PerfectLabyrinth.new_labyrinth 5 5
-  in PerfectLabyrinth.print laby
-
-let _ = main ()
--- wall.mli
+++
@@ -1,9 +0,0 @@
-module type WALL =
-sig
-  type t
-
-  val state : t -> bool
-  val flip : t -> unit
-end
-
-module SimpleWall : WALL
